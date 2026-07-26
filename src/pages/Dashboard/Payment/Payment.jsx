c9e1074539f254c2ce790d24c800022f3a371371
Jahid Hasan <jahidjubaer17@gmail.com>
2026-07-26 14:34:15 +0600

    fix: use canonical payment checkout endpoint

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -26,7 +26,7 @@ const Payment = () => {
             parcelName: request.parcelName
         }
 
-        const res = await axiosSecure.post('/create-checkout-session', paymentInfo);
+        const res = await axiosSecure.post('/payment-checkout-session', paymentInfo);
 
         console.log(res.data);
 
